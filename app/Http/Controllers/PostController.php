28f3abc7a8da54ac8ba6acf17cad5c22404c613a
orenogithubtakanori <tknr0807@gmail.com>
2024-01-07 09:32:40 +0000

    ブログ投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts'=> $post->getPaginateByLimit(5)]);
+        return view('posts.index')->with(['posts'=> $post->getPaginateByLimit(5)]);
         //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
+    
     public function show(Post $post)
     {
-        return view('posts/show')->with(['post' => $post]);
+        return view('posts.show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+         $input = $request['post'];
+         $post->fill($input)->save();
+         return redirect('/posts/' . $post->id);
+        //dd($request->all());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/',[PostController::class,'index']);
-// Route::get('/', function() {
-//     return view('posts.index');
-// });
-
-Route::get('/posts/{post}', [PostController::class ,'show']);
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']); //'/posts/{post}'のルーティングより上に書かないと、'/posts/{post}のshowメソッドが実行されてしまい想定外の挙動になる'
+Route::get('/posts/{post}', [PostController::class, 'show']); // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::post('/posts', [PostController::class, 'store']);
