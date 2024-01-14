cf5314c3a53cc53b08d2ae6ba72228427011914d
orenogithubtakanori <tknr0807@gmail.com>
2024-01-14 03:44:29 +0000

    ミス

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
             @foreach($posts as $post)
             <div class = 'post'>
                 <h2 class = 'title'><a href='posts/{{$post->id}}'>{{$post->title}}</a></h2>
-                <p class = 'body'>{{$post->body}}</p>
+                <p class = 'body'>{{$post-gitche>body}}</p>
                 <form action="/posts/{{$post->id}}" id="form_{{$post->id}}" method="post">
                     @csrf
                     @method('DELETE')
